ee5a215b33c814598d40cb7140752480aedbf457
Paloma <casagrande.paloma@gmail.com>
2022-05-16 13:05:55 -0300

    feat correção detalhes e criação de rotas

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,32 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../assets/css/reset.css";
 import "../assets/css/styles.css";
 import FlashcardPage from "./FlashcardPage";
 import logo from "../assets/img/logo.png"
 
 
-
 export default function App(){
-    const [clicked, setClicked]= React.useState(false);
 
     return(
-        <>
-            {clicked ?
-            (
-                <FlashcardPage/>
-            ) : (
-                <div className="logo">
-                    <img src={logo} alt="Logo" />
-                    <h1>ZapRecall</h1>
-                    <button onClick={()=> setClicked(true)}> Inicial Recall!</button>
-                </div>
-            )}
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<WelcomePage/>}/>
+                <Route path="/flashcards" element={<FlashcardPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
+function WelcomePage(){
+    return(
+        <div className="logo">
+            <img src={logo} alt="Logo" />
+            <h1>ZapRecall</h1>
+            <Link to={"/flashcards"}>
+                <button > Inicial Recall!</button>
+            </Link>
+        </div>
+    );
+};
